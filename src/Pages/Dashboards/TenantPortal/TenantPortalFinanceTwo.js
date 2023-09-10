7f30c28626a482869bec979b00d4447efe844cc5
sam002696 <sadmansakib221@gmail.com>
2023-09-10 10:45:03 +0600

    added changes in finances tab

--- src/Pages/Dashboards/TenantPortal/TenantPortalFinanceTwo.js
+++ src/Pages/Dashboards/TenantPortal/TenantPortalFinanceTwo.js
@@ -4,40 +4,37 @@ const TenantPortalFinanceTwo = () => {
   return (
     <>
       <div className="bg-white">
-        <div className="relative px-6 pt-8 lg:px-8">
-          <div className="mx-auto max-w-5xl py-4 sm:py-4 lg:py-4">
-            <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-              {/* <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                Announcing our next round of funding.{" "}
-                <a href="#" className="font-semibold text-indigo-600">
-                  <span className="absolute inset-0" aria-hidden="true" />
-                  Read more <span aria-hidden="true">&rarr;</span>
-                </a>
-              </div> */}
-            </div>
-            <div className="text-center">
-              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-                Nuova Property Finance
-              </h1>
-              <p className="mt-12 text-lg leading-8 text-gray-600">
-                Here at Nuova Property Management & Letting we use PayProp which
-                is a leading payment and reconciliation platform for the
-                residential lettings industry that automates the rental
-                payments.
-              </p>
-              <p className="mt-6 text-lg leading-8 text-gray-600">
-                Below you can click on the link to sign in to your account where
-                you will have a complete overview of you rental payments.
-              </p>
-
-              <div className="mt-10 flex items-center justify-center gap-x-6">
-                <a
-                  href="https://uk.payprop.com/tenant/login"
-                  className="rounded-md bg-indigo-600 px-3.5 py-3.5 text-md font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Visit Payprop
-                </a>
-              </div>
+        <div className="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
+          <div className="mx-auto max-w-6xl text-center shadow-xl p-5 rounded shadow-cyan-500/40">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl pb-3">
+              Nuova Property Finance
+            </h2>
+            <p className="mx-auto mt-6  text-lg leading-8 text-gray-600">
+              Here at Nuova Property Management & Letting we use PayProp which
+              is a leading payment and reconciliation platform for the
+              residential lettings industry that automates the rental payments.
+            </p>
+            <p className="mx-auto mt-6  text-lg leading-8 text-gray-600">
+              PayProp allows you to have a complete oversight of your properties
+              in one place, to compare rents and costs, quickly and easily.
+            </p>
+            <p className="mx-auto mt-6  text-lg leading-8 text-gray-600">
+              With the PayProp Owner App landlords can see if the rent has been
+              paid, income on their portfolio, third-party payment status and
+              much more.
+            </p>
+            <p className="mx-auto mt-6  text-lg leading-8 text-gray-600">
+              Follow the link below to download the App and your property
+              manager will help you set all of this up.
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              <a
+                href="https://uk.payprop.com/features/owner-app"
+                target="_blank"
+                className="rounded-md bg-cyan-600 px-3.5 py-3.5 text-md font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600"
+              >
+                Visit Payprop
+              </a>
             </div>
           </div>
         </div>
